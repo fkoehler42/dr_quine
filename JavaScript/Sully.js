47257c1256fb211472b94e2c0b55d1a1b142ad98
Flavien <fkoehler@student.42.fr>
2017-09-05 15:54:28 +0200

    fixed first replica variable decrement in Sully

--- JavaScript/Sully.js
+++ JavaScript/Sully.js
@@ -1,11 +1,8 @@
-const fs = require('fs')
-const {execSync} = require('child_process')
-const i = 5
-const replica = () => {
-	const fileName = 'Sully_'+i+'.js'
-	const str = 'const fs = require(\'fs\')\nconst {execSync} = require(\'child_process\')\nconst i = '+(i-1)+'\nconst replica = '+replica+'\nreplica()\n'
+const fs=require('fs');const{execSync}=require('child_process');let i=5
+const replica= () => {
+	if(__filename.indexOf('Sully.js')<0)--i;const fileName='Sully_'+i+'.js';const str='const fs=require(\'fs\');const{execSync}=require(\'child_process\');let i='+i+'\nconst replica= '+replica+'\nreplica()\n'
 	fs.writeFile(fileName, str, (err) => {
-		if (i>0)execSync('node '+fileName)
+		if(i>0)execSync('node '+fileName)
 	})
 }
 replica()
