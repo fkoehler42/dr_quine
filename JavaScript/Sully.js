2640d0efbafc2e6c082c997eaaee32119553c81c
Flavien <fkoehler@student.42.fr>
2017-09-12 13:30:53 +0200

    fixed Sully with i set to negative value

--- JavaScript/Sully.js
+++ JavaScript/Sully.js
@@ -1,6 +1,6 @@
 const fs=require('fs');const{execSync}=require('child_process');let i=5
 const replica= () => {
-	if(__filename.indexOf('Sully.js')<0)--i;const fileName='Sully_'+i+'.js';const str='const fs=require(\'fs\');const{execSync}=require(\'child_process\');let i='+i+'\nconst replica= '+replica+'\nreplica()\n'
+	if(i<0)return;if(__filename.indexOf('Sully.js')<0)--i;const fileName='Sully_'+i+'.js';const str='const fs=require(\'fs\');const{execSync}=require(\'child_process\');let i='+i+'\nconst replica= '+replica+'\nreplica()\n'
 	fs.writeFile(fileName, str, (err) => {
 		if(i>0)execSync('node '+fileName)
 	})
--- src/Sully.c
+++ src/Sully.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define STR "#include<stdio.h>%c#include<stdlib.h>%c#include<string.h>%c#define STR %c%s%c%cint main(){int i=%d;char bin[64];char src[64];char cmd[1000];if(!strstr(__FILE__,%cSully.c%c))--i;sprintf(bin,%cSully_%%d%c,i);sprintf(src,%cSully_%%d.c%c,i);%cFILE*fs=fopen(src,%cw%c);fprintf(fs,STR,10,10,10,34,STR,34,10,i,34,34,34,34,34,34,10,34,34,10,34,34,34,34,10);fclose(fs);%csprintf(cmd,%cgcc -Wextra -Werror -Wall %%s -o %%s%c,src,bin);system(cmd);if(i>0){sprintf(cmd,%c./%%s%c,bin);system(cmd);}}%c"
-int main(){int i=5;char bin[64];char src[64];char cmd[1000];if(!strstr(__FILE__,"Sully.c"))--i;sprintf(bin,"Sully_%d",i);sprintf(src,"Sully_%d.c",i);
+#define STR "#include<stdio.h>%c#include<stdlib.h>%c#include<string.h>%c#define STR %c%s%c%cint main(){int i=%d;char bin[64];char src[64];char cmd[1000];if(i<0)return(0);if(!strstr(__FILE__,%cSully.c%c))--i;sprintf(bin,%cSully_%%d%c,i);sprintf(src,%cSully_%%d.c%c,i);%cFILE*fs=fopen(src,%cw%c);fprintf(fs,STR,10,10,10,34,STR,34,10,i,34,34,34,34,34,34,10,34,34,10,34,34,34,34,10);fclose(fs);%csprintf(cmd,%cgcc -Wextra -Werror -Wall %%s -o %%s%c,src,bin);system(cmd);if(i>0){sprintf(cmd,%c./%%s%c,bin);system(cmd);}}%c"
+int main(){int i=5;char bin[64];char src[64];char cmd[1000];if(i<0)return(0);if(!strstr(__FILE__,"Sully.c"))--i;sprintf(bin,"Sully_%d",i);sprintf(src,"Sully_%d.c",i);
 FILE*fs=fopen(src,"w");fprintf(fs,STR,10,10,10,34,STR,34,10,i,34,34,34,34,34,34,10,34,34,10,34,34,34,34,10);fclose(fs);
 sprintf(cmd,"gcc -Wextra -Werror -Wall %s -o %s",src,bin);system(cmd);if(i>0){sprintf(cmd,"./%s",bin);system(cmd);}}
